e84bf92d43c9e31bbdcb1a496e5f1aafb66c7064
Samuel R. Meier <srm@aspen.sgc.loc>
2014-03-20 00:38:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+aspen_demo
+==========
+
+Simple demo scripts for the Aspen Workshop
---
+++ qsub_example.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 fastq";
+fi
+if [ $# -ne 1 ]; then
+    echo "FASTQ file required"
+    exit 1
+fi
+
+BOWTIE='/home/srm/apps/bowtie/bowtie'
+BOWTIE_INDEXES='/n/projects/srm/genomes/current_genome/ensembl/release-75/mus_musculus/bowtie_indexes/'
+GENOME="${BOWTIE_INDEXES}Mus_musculus.GRCm38.75.min"
+
+FASTQ=$1
+BAM=$(basename ${FASTQ} ".fastq.gz")".bam"
+CPU=8
+JOB_ID="$(basename ${BAM} .bam)"
+
+job="${BOWTIE} -S -p ${CPU} ${GENOME} <(gunzip -c ${FASTQ}) --chunkmbs 512 -m 1 -k 1 -n 2 --best --strata | samtools view -F 4 -Sbo ${BAM} - "
+
+qsub -N ${JOB_ID} -j y -pe by_node $CPU -cwd -M ${USER}@stowers.org -m ae -b y "$job"
+
