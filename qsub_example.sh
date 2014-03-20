4a311a1e263ab5e8096e39e9072267413b841fb6
Meier <srm@mp00bmdjwt.sgc.loc>
2014-03-20 11:23:44 -0500

    fixed bowtie to use env bowtie

--- qsub_example.sh
+++ qsub_example.sh
@@ -7,14 +7,14 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-BOWTIE='/home/srm/apps/bowtie/bowtie'
+BOWTIE=$(which bowtie)
 BOWTIE_INDEXES='/n/projects/srm/genomes/current_genome/ensembl/release-75/mus_musculus/bowtie_indexes/'
 GENOME="${BOWTIE_INDEXES}Mus_musculus.GRCm38.75.min"
 
 FASTQ=$1
 BAM=$(basename ${FASTQ} ".fastq.gz")".bam"
 CPU=8
-JOB_ID="$(basename ${BAM} .bam)"
+JOB_ID="$(basename ${BAM} .bam)_${USER}"
 
 job="${BOWTIE} -S -p ${CPU} ${GENOME} <(gunzip -c ${FASTQ}) --chunkmbs 512 -m 1 -k 1 -n 2 --best --strata | samtools view -F 4 -Sbo ${BAM} - "
 
